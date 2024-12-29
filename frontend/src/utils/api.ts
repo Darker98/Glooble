05826162a09102702790230abad0405eec68aaae
Darker98 <96902917+Darker98@users.noreply.github.com>
2024-12-29 20:28:27 +0500

    Fixed some merge issues

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -23,7 +23,7 @@ export const uploadArticle = async (article: SearchResult): Promise<boolean> => 
 };
 
 export const searchArticles = async (query: string, page: number, perPage: number): Promise<Response> => {
-  return fetch('http://127.0.0.1:5000/query', {
+  return fetch('http://127.0.0.1:8080/query', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
